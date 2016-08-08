cddc19079a42dceaf2810e08ecdf3477ca86564e
By Robots <skynet@by-robots.com>
2016-08-08 20:53:14 +0100

    Upload paths.

--- config/colourise.php
+++ config/colourise.php
@@ -7,4 +7,18 @@ return [
      * @var string
      */
     'name' => 'Colourise',
+
+    /**
+     * Path to store originals.
+     *
+     * @var string
+     */
+    'original-path' => storage_path() . '/images/original',
+
+    /**
+     * Path to store the colourised photos.
+     *
+     * @var string
+     */
+    'colourised-path' => storage_path() . '/images/colourised',
 ];
